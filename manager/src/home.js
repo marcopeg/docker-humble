9a6dbf962700b0bad3864a0ed7e10a6703ba1454
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-10-17 16:51:07 +0200

    Hello world is working

---
+++ manager/src/home.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                Hello World*
+            </div>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
