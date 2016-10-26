ede68a4a2203358d3452596dd835786141daae65
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-10-26 14:45:52 +0200

    Minor fixes

--- examples/server-api/server-api/services/api/routes/proxy.js
+++ examples/server-api/server-api/services/api/routes/proxy.js
@@ -6,14 +6,15 @@ const liveCache = require('../libs/live-cache');
 const router = express.Router();
 module.exports = router;
 
-const runAction = actionName => (req, res) =>
+const runAction = actionName => (req, res) => {
+    liveCache.stop();
     (new Cmd('./humble-server ' + actionName)).exec()
         .then(() => {
-            liveCache.refresh();
+            liveCache.start();
             res.send('+ok');
         })
-        .catch(err => res.status(500).send(err));
-
+        .catch(err => res.status(500).send(err))
+};
 
 ['up', 'down', 'start', 'stop', 'reload'].forEach(actionName => {
     router.post('/' + actionName, runAction(actionName));
